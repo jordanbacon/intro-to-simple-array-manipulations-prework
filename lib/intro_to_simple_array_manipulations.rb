96988c6a626894c528d465e5b6b0a9a260ec3568
Jordan Bacon <jmbacon92@gmail.com>
2019-06-20 23:31:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,19 +27,5 @@ def using_concat(array1, array2)
 end
 
 def using_insert(array, example)
-  
-
- before(:each) do 
-    @list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
-    @another_esoteric_language = "Malbolge"
-    @new_array = using_insert(@list_of_esoteric_programming_languages, @another_esoteric_language)
-  end
-
-  it "takes in two arguments, an array and a new element to be added to the array. it uses the insert method to add the new element to the 4th index of the array" do 
-    expect(@new_array[4]).to eq(@another_esoteric_language)
-  end
-
-  it "increases the length of the array" do 
-    expect(@new_array.length).to eq(8)
-  end
+  array.insert(4, example)
 end
