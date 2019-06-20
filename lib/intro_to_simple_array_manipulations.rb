065e15ff1011f2740652eb52934e621470bf1111
Jordan Bacon <jmbacon92@gmail.com>
2019-06-20 23:33:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,3 +29,14 @@ end
 def using_insert(array, example)
   array.insert(4, example)
 end
+
+def using_uniq(array)
+  
+
+describe "#using_uniq" do 
+  it "takes in an argument of an array and uses the uniq method to remove any duplicate items" do
+    captain_planet_and_the_planeteers = ["Captain Planet", "Gaia", "Kwame", "Gi", "Linka", "Wheeler", "Gaia"]
+    new_array = using_uniq(captain_planet_and_the_planeteers)
+    expect(new_array).to eq(["Captain Planet", "Gaia", "Kwame", "Gi", "Linka", "Wheeler"])
+  end
+end
